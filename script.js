38d2d849a097ac8498cd31f9ae72f1f454b63f94
JairoCamacho <jairosistemas100@gmail.com>
2024-10-05 15:06:09 -0400

    parte1

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const boton = document.createElement('button');
-    boton.textContent = "Descubre más sobre los exoplanetas";
-    document.body.appendChild(boton);
-
-    boton.addEventListener('click', function() {
-        alert('Los exoplanetas son fascinantes porque...');
-    });
-});
